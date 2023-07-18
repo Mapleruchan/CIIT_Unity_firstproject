e8b0a6427341e481a2116b9127a04b682e79def9
Mapleruchan <134357878+Mapleruchan@users.noreply.github.com>
2023-07-18 13:28:57 +0800

    1

--- Assets/Scenes/scripts/text gae m manager.cs
+++ Assets/Scenes/scripts/text gae m manager.cs
@@ -7,7 +7,7 @@ public class textgaemmanager : MonoBehaviour
     public TextMeshProUGUI storyTextMeshPro, hpTextMeshPro, staminaTextMeshPro;
     public string storytext;
     public int hp, stam;
-    public GameObject lv1choice,lv2choice,helt,stamy;
+    public GameObject next,lv1choice,lv2choice,helt,stamy;
     
     // Start is called before the first frame update
     void Start()
@@ -28,7 +28,7 @@ public class textgaemmanager : MonoBehaviour
     public void go()
     {
         lv1choice.SetActive(true);
-        lv2choice.SetActive(true);
+       
         stamy.SetActive(true);
         helt.SetActive(true);
     }
@@ -40,20 +40,30 @@ public class textgaemmanager : MonoBehaviour
         storytext = "!You killed a bunch of them but you ran out of ammo, you got bitten... Good luck";
         lv1choice.SetActive(false);
         hp -= 3;
-        lv2choice.SetActive(true);
+        next.SetActive(true);
     }
     public void Nuke()
     {
         storytext = "!You killed all of them but blasted yourself in the process";
         lv1choice.SetActive(false);
         hp -= 7;
-        lv2choice.SetActive(true);
+        next.SetActive(true);
     }
     public void Grenade()
     {
         storytext = "!You fired your only grenade... it didn't hit much, their coming for you, suddenly the gas stations start exploding, killing all the zombies. You were so lucky that you were unscathed";
         lv1choice.SetActive(false);
-        lv2choice.SetActive(true);
+        next.SetActive(true);
 
     }
+
+    public void onward()
+    {
+        storytext = "!A POISONOUS MONSTER SHOWS UP, WHAT DO YOU DO?";
+        lv2choice.SetActive(true);
+        next.SetActive(false);
+
+    }
+
+
 }
